814eb4f96daad59e859ffc520e008f78054bb751
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:03:40 -0700

    Add files via upload

---
+++ Reverse numeros entre 1 a 999 .py
@@ -0,0 +1,12 @@
+#Peyman Miyandashti 250161
+#numero mas Grande que otro numero 
+x=int(input("engresar numerode x:"))
+y=int(input("engresar numero de y:"))
+      
+if x<y:
+    print("y es grande que x")
+elif x == y:
+    print("Error")
+else:
+    x>y 
+    print("x es grande que y")
