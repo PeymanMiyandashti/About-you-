8f0d003ea23ed1c7d8b27bd8e745dbca9fcce231
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 18:59:31 -0700

    Add files via upload

---
+++ append.py
@@ -0,0 +1,7 @@
+#peyman Miyandashti 250161 
+#add another selection inside the list 
+
+fruits = ["apple", "banana"]
+fruits.append("cherry")
+
+print(fruits)  # Output: ['apple', 'banana', 'cherry']
