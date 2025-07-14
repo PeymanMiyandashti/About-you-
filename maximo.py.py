532dc287479e03bf0b4e3f35200547a012e47a51
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:01:49 -0700

    Add files via upload

---
+++ maximo.py.py
@@ -0,0 +1,14 @@
+#PeymanMiyandashti 250161
+#Algoritmo de calcula las operaciones basicas(+-*/)de dos numeros 
+x=10
+y=2
+#calcularr + * - /
+sum = x+y
+res=x-y
+mul=x*y
+div= x/y
+#imprimir la suma y el promedia 
+print(f"la operacion {x}+{y} es:{sum}")
+print(f"la operacion {x}-{y} es:{res}")
+print(f"la operacion {x}*{y} es:{mul}")
+print(f"la operacion {x}/{y} es:{div:.0f}")
