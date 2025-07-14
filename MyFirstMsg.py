944525521ab6d071a7fe25428f3e069a881146f5
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:02:41 -0700

    Add files via upload

---
+++ MyFirstMsg.py
@@ -0,0 +1,29 @@
+
+
+
+#Peyman Miyandashti 250161
+#printing my  and my first msg in python
+line0 = ('')
+line1 = "************************************************"
+line2 = "*                                              *"
+line3 = "*                                              *"
+line4 = "*                                              *"
+line5 = "*   ! WELCOME TO MY LITTLE CODE WORLD !        *"
+line6 = "*   ! THIS IS PEYMAN MIYANDASHTI      !        *"
+line7 = "*   ! THIS IS MY FIRST PYTHON PROGRAM !        *"
+line8 = "*                                              *"
+line9 = "*                                              *"
+line2 = "*                                              *"
+line1 = "************************************************"
+print(line0)
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+print(line5)
+print(line6)
+print(line7)
+print(line8)
+print(line9)
+print(line2)
+print(line1)
