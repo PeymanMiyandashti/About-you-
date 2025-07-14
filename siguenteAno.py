8d818134559bd88491fa5542722a2092de7a9403
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:04:21 -0700

    Add files via upload

---
+++ siguenteAno.py
@@ -0,0 +1,7 @@
+#Peyman Miyandashti 250161
+# El ano que viene cuanto edad tienes 
+
+nombre =input('Engresar Tun Nombre:')
+print("Holla",nombre)
+edad=int(input('Engresar Tu Edad'))
+print("El año que viene tendrás",edad+1)
