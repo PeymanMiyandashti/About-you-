cf9b082a96ef34e24137ca3e059e9cf59af969ea
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:00:38 -0700

    Add files via upload

---
+++ AreaDeRectangulo.py.py
@@ -0,0 +1,11 @@
+#Peyman Miyandashti 250161 
+#Suma de 3 numeros 
+num1=float(input("ingrese el num1:"))
+num2=float(input("ingrese el num2:"))
+num3=float(input("ingrese el num3:"))
+#calcular 
+suma=num1+num2+num3
+#imprimir
+
+print(f"El suma de 3 numero  :{num1} y  {num2} y {num3} es:{suma:.0f}")
+#fin
