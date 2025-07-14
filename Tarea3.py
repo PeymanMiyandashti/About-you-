4390aab205477e0cd34d76aecc716c8d360123fb
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:07:09 -0700

    Add files via upload

---
+++ Tarea3.py
@@ -0,0 +1,22 @@
+#Peyman Miyandashti 250161
+#algoritmo para ver la pelicula tiburon
+
+
+#INICIO
+#1. Encender el dispositivo (televisión, computadora, etc.)
+#2. Abrir la aplicación de streaming o insertar el DVD
+#3. Buscar la película "Tiburón"
+#4. Seleccionar la película
+#5. Ajustar volumen y pantalla
+#6. Reproducir la película
+#7. Disfrutar la película
+#FIN
+
+
+print("1. Encender el dispositivo (TV, computadora, etc.)")
+print("2. Abrir la aplicación de streaming como Netflix")
+print("3. Buscar la película 'Tiburón'")
+print("4. Seleccionar la película")
+print("5. Ajustar volumen y pantalla")
+print("6. Reproducir la película")
+print("7. ¡Disfrutar la película!")
