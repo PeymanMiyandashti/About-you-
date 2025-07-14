12151aebcdb468eb73fbd381632b530f7bb67f3f
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 18:57:33 -0700

    Add files via upload

---
+++ AboutYou.py
@@ -0,0 +1,15 @@
+#peyman Miyandashti 250161
+#Section about you in application
+name='Peyman'
+age=42
+height=186
+city ="Mexicali"
+papulation= 1_020_000
+is_student=True
+print("name:",name)
+print("Age:",age)
+print("Height:",height)
+print("Is Student?",is_student)
+print("Hello Peyman!")
+print(name, "is" , age, "years old")
+print(city,"has a population of ",papulation)
