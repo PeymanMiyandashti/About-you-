e7f65e7cd4c5b18464c7063e1fcac2b87a7451ea
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:04:07 -0700

    Add files via upload

---
+++ signosopuestos.py
@@ -0,0 +1,8 @@
+#Peyman Miyandashti 250161
+#SignosOpuestos 
+num1=float(input("ingrese el primer numero:"))
+num2=float(input("ingrese el segundo numero:"))
+if num1>0 and num2>0 or num1<0 and num2<0:
+    print("No son opuestos")
+else:
+    print("Signos opuestos")
