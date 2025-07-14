576472fca6603b186cca1492102c1b21b3184458
PeymanMiyandashti <peayman.tabriz@yahoo.com>
2025-07-13 19:06:25 -0700

    Add files via upload

---
+++ Tarea2.py
@@ -0,0 +1,21 @@
+#Peyman Miyandashti 250161
+#Algoritmo y flujograma para colocarnos una camisa
+#INICIO
+#1. Tomar la camisa
+#2. Verificar que esté al derecho
+#3. Introducir un brazo en la manga correspondiente
+#4. Introducir el otro brazo en la otra manga
+#5. Ajustar la camisa sobre los hombros
+#6. Abotonar (si tiene botones)
+#7. Acomodar la camisa
+
+#FIN
+
+
+print("1. Tomar la camisa")
+print("2. Verificar que esté al derecho")
+print("3. Introducir un brazo en la manga correspondiente")
+print("4. Introducir el otro brazo en la otra manga")
+print("5. Ajustar la camisa sobre los hombros")
+print("6. Abotonar la camisa (si tiene botones)")
+print("7. Acomodar la camisa")
